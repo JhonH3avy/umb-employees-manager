5377abea3ad225a5f7ad2f9c764408d86b784729
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2020-08-28 09:07:44 -0500

    use all controller in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,5 @@
+import { DepartmentController } from './controllers/department.controller';
+import { CompanyController } from './controllers/company.controller';
 import { EmployeeService } from './services/employee.service';
 import { EmployeeController } from './controllers/employee.controller';
 import env from 'dotenv';
@@ -5,6 +7,8 @@ env.config();
 import { DbContext } from './services/db-context';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { CompanyService } from './services/company.service';
+import { DepartmentService } from './services/department.service';
 
 const context = new DbContext();
 const app = express();
@@ -16,7 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const dbContext = new DbContext();
 
+const companyController = new CompanyController(new CompanyService(dbContext));
+const departmentController = new DepartmentController(new DepartmentService(dbContext));
 const employeeController = new EmployeeController(new EmployeeService(dbContext));
+
+app.get('/company', companyController.companies);
+app.get('/company/:key', companyController.company);
+app.post('/company', companyController.create);
+app.put('/company/:key', companyController.update);
+
+app.get('/department', departmentController.departments);
+app.get('/department/:key', departmentController.department);
+app.post('/department', departmentController.create);
+app.put('/department/:key', departmentController.update);
 
 app.get('/employee', employeeController.employees);
 app.get('/employee/:key', employeeController.employee);
